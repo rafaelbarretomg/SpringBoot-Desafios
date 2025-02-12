d12a85bf689bc3550c8c9b1a15f9b18f3aa69885
Rafael Barreto <86436697+rafaelbarretomg@users.noreply.github.com>
2025-02-12 01:32:27 -0300

    Add files via upload
    Inicio desafio 03

---
+++ challenge03-gestao-pedidos/src/main/java/br/developer/rafael/challenge03_gestao_pedidos/Challenge03GestaoPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.developer.rafael.challenge03_gestao_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge03GestaoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge03GestaoPedidosApplication.class, args);
+	}
+
+}
---
+++ challenge03-gestao-pedidos/src/test/java/br/developer/rafael/challenge03_gestao_pedidos/Challenge03GestaoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.developer.rafael.challenge03_gestao_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge03GestaoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
